2edd93b8c17762ec1b536601c71a710ee1dc9114
herve <h.fontbonne@treeptik.fr>
2014-01-14 18:21:43 +0100

    test

---
+++ init-db-cloudunit.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE IF NOT EXISTS `ToDo` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`date` datetime DEFAULT NULL,
+`name` varchar(255) DEFAULT NULL,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+INSERT INTO `ToDo` (`id`, `date`, `name`) VALUES
+(1, '2013-04-18 00:00:00', 'testInit'),
+(2, '2013-09-17 00:00:00', 'testInit2');
